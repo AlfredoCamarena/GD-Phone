c6dd691c4893d69ce8fef0a066b3f01b63a091d1
YeyoGSD <yeyoGSD@proton.me>
2026-01-03 20:22:11 -0700

    Add chat interaction loop (send and recieve)

--- Apps/1_Chat/ChatDetail/chat_detail.gd
+++ Apps/1_Chat/ChatDetail/chat_detail.gd
@@ -9,12 +9,19 @@ const BUBBLE_OTHER = preload("uid://dcqg1uuswi4y8")
 @onready var back_button: Button = %BackButton
 @onready var avatar: TextureRect = %Avatar
 @onready var message_scroll: ScrollContainer = %MessageScroll
+@onready var type_box: LineEdit = %TypeBox
+@onready var send_button: Button = %SendButton
 
 signal requested_go_back
 
 func _ready() -> void:
 	back_button.pressed.connect(func() -> void:
 		requested_go_back.emit())
+	
+	type_box.text_submitted.connect(func(_text: String) -> void:
+		_on_send_pressed())
+	
+	send_button.pressed.connect(_on_send_pressed)
 
 
 func setup(chat_data: ChatData) -> void:
@@ -28,7 +35,7 @@ func setup(chat_data: ChatData) -> void:
 	for msg in chat_data.conversation:
 		add_message_bubble(msg)
 	
-	message_scroll.scroll_vertical = message_scroll.get_v_scroll_bar().max_value as int
+	scroll_to_bottom()
 
 
 func add_message_bubble(msg_data: MessageData) -> void:
@@ -41,4 +48,39 @@ func add_message_bubble(msg_data: MessageData) -> void:
 		
 	messages_container.add_child(bubble)
 	bubble.setup(msg_data.text)
+
+
+func trigger_npc_reply() -> void:
+	# TODO: Agregar animación
+	await get_tree().create_timer(2.0).timeout
 	
+	var reply_msg := MessageData.new()
+	reply_msg.text = "¡Qué interesante! Cuéntame más." # TODO: Cambiar respuesta genérica
+	reply_msg.sender = MessageData.Sender.OTHER
+	add_message_bubble(reply_msg)
+	
+	scroll_to_bottom()
+
+
+func scroll_to_bottom() -> void:
+	# Espera un frame para que Godot recalcule el tamaño de la nueva burbuja
+	await get_tree().process_frame
+	message_scroll.set_deferred(&"scroll_vertical", message_scroll.get_v_scroll_bar().max_value )
+
+
+func _on_send_pressed() -> void:
+	var text := type_box.text.strip_edges()
+	if text.is_empty():
+		return
+	
+	var new_msg := MessageData.new()
+	new_msg.text = text
+	new_msg.sender = MessageData.Sender.ME
+	add_message_bubble(new_msg)
+	
+	type_box.text = ""
+	
+	scroll_to_bottom()
+	
+	# TODO: QUITAR SIMULACIÓN TEMPORAL
+	trigger_npc_reply()
