b78243f267ab9305fa330e57e301f6fc58544893
Alfredo Camarena <alfredoccm@protonmail.com>
2026-01-05 00:42:57 -0700

    Add PhotoData resource to hold image metadata

--- Apps/2_Gallery/PhotoThumbnail/photo_thumbnail.gd
+++ Apps/2_Gallery/PhotoThumbnail/photo_thumbnail.gd
@@ -1,5 +1,10 @@
 class_name PhotoThumbnail
 extends TextureButton
 
-func setup(photo: Texture2D) -> void:
-	texture_normal = photo
+var photo_data: PhotoData
+
+func setup(data: PhotoData, cell_width: float) -> void:
+	photo_data = data
+	texture_normal = data.image
+	
+	custom_minimum_size = Vector2(cell_width, cell_width)
