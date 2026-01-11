3719c19dfe1e33f73bb8e9930c7a602a6c357cf6
Alfredo Camarena <alfredoccm@protonmail.com>
2026-01-11 14:38:18 -0700

    Add notifications

--- _System/_Autoloads/player_data.gd
+++ _System/_Autoloads/player_data.gd
@@ -8,3 +8,6 @@ func add_photo(photo: PhotoData) -> void:
 	if not photo in unlocked_photos:
 		unlocked_photos.append(photo)
 		photo_unlocked.emit(photo)
+		
+		var new_photo_notification := preload("uid://dcb0bjeeoa2re")
+		EventManager.trigger_notification.emit(new_photo_notification)
